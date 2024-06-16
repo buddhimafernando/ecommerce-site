3816b10a5d2b4d697eada51714865abff1aea021
Buddhima Fernando <harun.fernando.hf@gmail.com>
2024-06-16 19:13:27 +0530

    feat: Add order, layout, landing, customer, and products components
    The code changes include adding new components for order, layout, landing, customer, and products. These components have been added to their respective HTML and TypeScript files. Additionally, unit tests have been added for each component.
    
    Note: The recent user commits and repository commits do not provide any additional context for the commit message.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CartComponent } from './pages/admin/cart/cart.component';
 import { CategoriesComponent } from './pages/admin/categories/categories.component';
+import { CustomerComponent } from './pages/admin/customer/customer.component';
+import { LayoutComponent } from './pages/admin/layout/layout.component';
+import { OrderComponent } from './pages/admin/order/order.component';
+import { ProductsComponent } from './pages/admin/products/products.component';
+import { LandingComponent } from './pages/website/landing/landing.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CartComponent,
-    CategoriesComponent
+    CategoriesComponent,
+    CustomerComponent,
+    LayoutComponent,
+    OrderComponent,
+    ProductsComponent,
+    LandingComponent,
   ],
   imports: [
     BrowserModule,
